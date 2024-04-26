f9d2f2d20c767c0ee99984e9dec0af0a4660c8d3
BenJCross1995 <92743455+BenJCross1995@users.noreply.github.com>
2024-04-26 01:34:08 +0100

    Add code to read and write jsonl files

---
+++ code/read_and_write_docs.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import json
+import pandas as pd
+
+def read_jsonl_file(file_path):
+    data = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            data.append(json.loads(line))
+            
+    data = pd.DataFrame(data)
+    return data
+
+def save_as_jsonl(data, file_path):
+    with open(file_path, 'w') as file:
+        for _, row in data.iterrows():
+            json.dump(row.to_dict(), file)
+            file.write('\n')
